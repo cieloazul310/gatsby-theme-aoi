a2f42cd90b36ea4b439d3af6ac0fb23359fb113a
cieloazul310 <cieloazul310@gmail.com>
2021-02-26 11:21:44 +0900

    #2: update

--- src/layout/BottomNav/BottomNavItem.tsx
+++ src/layout/BottomNav/BottomNavItem.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
-import { Link as GatsbyLink, withPrefix } from 'gatsby';
+import { Link as GatsbyLink } from 'gatsby';
 import BottomNavigationAction, { BottomNavigationActionProps } from '@material-ui/core/BottomNavigationAction';
 
 type Props = BottomNavigationActionProps;
 
-function BottomNavItem({ label, value, icon, showLabel, selected, ...props }: Props): JSX.Element {
-  console.log(props);
+function BottomNavItem({ label, value, icon, showLabel, selected }: Props): JSX.Element {
   return <BottomNavigationAction label={label} component={GatsbyLink} to={value} icon={icon} selected={selected} showLabel={showLabel} />;
 }
 
--- src/layout/BottomNav/index.tsx
+++ src/layout/BottomNav/index.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
-// import { navigate, withPrefix } from 'gatsby';
+import { withPrefix } from 'gatsby';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
-// import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
 // icons
 import Home from '@material-ui/icons/Home';
@@ -9,29 +8,22 @@ import MusicNote from '@material-ui/icons/MusicNote';
 import { useLocation } from '@reach/router';
 import BottomNavItem from './BottomNavItem';
 
-const useStyles = makeStyles((theme) => createStyles({
-  root: {
-    borderTop: `1px solid ${theme.palette.divider}`,
-  },
-}));
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      borderTop: `1px solid ${theme.palette.divider}`,
+    },
+  })
+);
 
 function BottomNav(): JSX.Element {
   const { pathname } = useLocation();
   const classes = useStyles();
-  console.log(pathname);
-  /*
-  const onChange = (event: React.ChangeEvent<Record<string, unknown>>, value: string) => {
-    navigate(value.replace(withPrefix('/'), '/'));
-  };
-  */
+
   return (
-    <BottomNavigation className={classes.root} value={pathname} showLabels>
+    <BottomNavigation className={classes.root} value={pathname.replace(withPrefix('/'), '/')} showLabels>
       <BottomNavItem label="Top" value="/" icon={<Home />} />
       <BottomNavItem label="Page2" value="/page-2/" icon={<MusicNote />} />
-      {/*
-        <BottomNavigationAction label="Top" value={withPrefix('/')} icon={<Home />} />
-      <BottomNavigationAction label="page2" value={withPrefix('/page-2/')} icon={<MusicNote />} />
-      */}
     </BottomNavigation>
   );
 }
